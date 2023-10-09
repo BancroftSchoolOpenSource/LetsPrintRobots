30eb004eeb3bf5cfcaf6dbf2e1e7a576ccf25d40
Kevin harrington <mad.hephaestus@gmail.com>
2023-10-09 15:48:07 -0400

    4 servo mode

--- LetsPrintRobots.ino
+++ LetsPrintRobots.ino
@@ -5,16 +5,16 @@
 #include <Esp32WifiManager.h>
 #include "wifi/WifiManager.h"
 #include "WebPage.h"
-
+#include <WiiChuck.h>
 
 Servo m1;
 Servo m2;
 Servo m3;
-
+Servo m4;
 WebPage control_page;
 
 WifiManager manager;
-
+Accessory nunchuck1;
 /*
  * This is the standard setup function that is called when the ESP32 is rebooted
  * It is used to initialize anything that needs to be done once.
@@ -57,33 +57,49 @@ WifiManager manager;
 int inc = 0;
 int timerTime = 0;
 void setup() {
-  ESP32PWM::allocateTimer(1);  // Used by servos
-  m1.attach(14);
-  m2.attach(27);
-  m3.attach(26);
+	ESP32PWM::allocateTimer(0);  // Used by servos
+	m1.attach(14);
+	m2.attach(25);
+	m3.attach(26);
+  m4.attach(12);
 
-  m1.write(90);
-  m2.write(90);
-  m3.write(90);
-
-  //manager.setup();  // Connect to an infrastructure network first, then fail over to AP mode
-  manager.setupAP();// Launch AP mode first, then fail over to connecting to a station
-  while (manager.getState() != Connected) {
-    manager.loop();
-    delay(1);
-  //   delay(1000);
-  //     m1.write(120);
-  // m2.write(120);
-  // m3.write(120);
-  //   delay(1000);
-  //     m1.write(90);
-  // m2.write(90);
-  // m3.write(90);
-  }
-  Serial.println("Wifi Connected!");
-
-
-  control_page.initalize();  // Init UI after everything else.
+	m1.write(90);
+	m2.write(90);
+	m3.write(90);
+  m4.write(90);
+	//manager.setup();  // Connect to an infrastructure network first, then fail over to AP mode
+	manager.setup(); // Launch AP mode first, then fail over to connecting to a station
+	// while (manager.getState() != Connected) {
+	// 	manager.loop();
+	// 	delay(10);
+	// 	//manager.printState();
+	// 	//   delay(1000);
+	// 	//     m1.write(120);
+	// 	// m2.write(120);
+	// 	// m3.write(120);
+	// 	//   delay(1000);
+	// 	//     m1.write(90);
+	// 	// m2.write(90);
+	// 	// m3.write(90);
+	// }
+	delay(100);
+	Serial.println("Wifi Connected!");
+	//control_page.initalize();  // Init UI after everything else.
+	Serial.println("Starting the Web Control");
+	timerTime=millis()+100;
+  nunchuck1.begin();
+	if (nunchuck1.type == Unknown) {
+		/** If the device isn't auto-detected, set the type explicatly
+		 * 	NUNCHUCK,
+		 WIICLASSIC,
+		 GuitarHeroController,
+		 GuitarHeroWorldTourDrums,
+		 DrumController,
+		 DrawsomeTablet,
+		 Turntable
+		 */
+		nunchuck1.type = NUNCHUCK;
+	}
 }
 
 /*
@@ -94,19 +110,52 @@ void setup() {
  * that is zeroed when the state begins. It is compared with the number of
  * milliseconds the robot should reamain in that state.
  */
+ float fmap(float x, float in_min, float in_max, float out_min, float out_max) {
+    const float run = in_max - in_min;
+    if(run == 0){
+        log_e("map(): Invalid input range, min == max");
+        return -1; // AVR returns -1, SAM returns 0
+    }
+    const float rise = out_max - out_min;
+    const float delta = x - in_min;
+    return (delta * rise) / run + out_min;
+}
 void runStateMachine() {
 
-  //float x = control_page.getJoystickX();
-  //float y = control_page.getJoystickY();
-  float slide = control_page.getSliderValue(0);
-  int offset = slide * 10 - 5;
+	float x =0;// control_page.getJoystickX();
+	float y = 0;//control_page.getJoystickY();
 
-  int m1val = offset + (90);
-  int m2val = offset + (90);
-  int m3val = offset + (90);
-  m1.write(m1val);
-  m2.write(m2val);
-  m3.write(m3val);
+	float slide =0;
+  nunchuck1.readData();    // Read inputs and update maps
+	// nunchuck1.printInputs(); // Print all inputs
+	// for (int i = 0; i < WII_VALUES_ARRAY_SIZE; i++) {
+	// 	Serial.println(
+	// 			"Controller Val " + String(i) + " = "
+	// 					+ String((uint8_t) nunchuck1.values[i]));
+      
+	// }
+  long buttons = 0;
+  long mouth = 90;
+  if(nunchuck1.values[10]>0){
+    buttons=255;
+  }
+  if(nunchuck1.values[11]>0){
+    mouth=120;
+  }
+  slide = fmap(buttons,0,255,-1.0,1.0);
+  x= fmap(nunchuck1.values[1],0,255,-1.0,1.0);
+  y= fmap(nunchuck1.values[0],0,255,-1.0,1.0);
+  //slide = control_page.getSliderValue(0);
+	int offset = slide * 20 ;
+  int scale =30;
+  //Serial.println(String(offset));
+	int m1val = offset + (90) +(x*-scale);
+	int m2val = offset + (90)+(x*scale*cos(-120))+(y*scale*sin(-120));
+	int m3val = offset + (90)+(x*scale*cos(-120))+(y*scale*sin(120));
+	m1.write(m1val);
+	m2.write(m2val);
+	m3.write(m3val);
+  m4.write(mouth);
 }
 
 /*
@@ -118,33 +167,28 @@ void runStateMachine() {
 
 uint32_t packet_old = 0;
 void updateDashboard() {
-  // This writes values to the dashboard area at the bottom of the web page
-  if ((timerTime + 100) > millis()) {
+	// This writes values to the dashboard area at the bottom of the web page
+	if ((timerTime + 100) > millis()) {
+		//Serial.println("Update dashboard");
+		control_page.setValue("Simple Counter", inc++);
+		//if(control_page.getJoystickMagnitude()>0.1)
+		//Serial.println("Joystick angle="+String(control_page.getJoystickAngle())+
+		//		" magnitude="+String(control_page.getJoystickMagnitude())+
+		//		" x="+String(control_page.getJoystickX())+
+		//						" y="+String(control_page.getJoystickY()) +
+		//						" slider="+String(control_page.getSliderValue(0)));
 
+		control_page.setValue("packets from Web to ESP",
+				control_page.rxPacketCount);
 
-    control_page.setValue("Simple Counter",
-                          inc++);
-    //if(control_page.getJoystickMagnitude()>0.1)
-    //Serial.println("Joystick angle="+String(control_page.getJoystickAngle())+
-    //		" magnitude="+String(control_page.getJoystickMagnitude())+
-    //		" x="+String(control_page.getJoystickX())+
-    //						" y="+String(control_page.getJoystickY()) +
-    //						" slider="+String(control_page.getSliderValue(0)));
+		control_page.setValue("Simple Counter", inc++);
+		control_page.setValue("packets from Web to ESP",
+				control_page.rxPacketCount);
+		control_page.setValue("packets to Web from ESP",
+				control_page.txPacketCount);
 
-    control_page.setValue("packets from Web to ESP",
-                          control_page.rxPacketCount);
-
-
-
-    control_page.setValue("Simple Counter",
-                          inc++);
-    control_page.setValue("packets from Web to ESP",
-                          control_page.rxPacketCount);
-    control_page.setValue("packets to Web from ESP",
-                          control_page.txPacketCount);
-
-    timerTime = millis();
-  }
+		timerTime = millis();
+	}
 }
 
 /*
@@ -155,8 +199,9 @@ void updateDashboard() {
 
 void loop() {
 
-  manager.loop();
-  runStateMachine();                    // do a pass through the state machine
-  if (manager.getState() == Connected)  // only update if WiFi is up
-    updateDashboard();                  // update the dashboard values
+	manager.loop();
+	runStateMachine();                    // do a pass through the state machine
+	//if (manager.getState() == Connected)  // only update if WiFi is up
+	//	updateDashboard();                  // update the dashboard values
+	delay(1);
 }
